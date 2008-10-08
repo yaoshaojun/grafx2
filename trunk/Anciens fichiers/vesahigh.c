d48ef4183908ed5321e9581d88577be6ef9cfcd1
yrizoud <yrizoud@6a45ab37-182e-0410-b566-3f6d2655a21a>
2008-10-08 19:40:47 +0000

    Copyright notices in source files

--- trunk/Anciens fichiers/vesahigh.c
+++ trunk/Anciens fichiers/vesahigh.c
@@ -1,3 +1,22 @@
+/*  Grafx2 - The Ultimate 256-color bitmap paint program
+
+    Copyright 1996-2001 Sunset Design (Guillaume Dorme & Karl Maritaud)
+
+    Grafx2 is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; version 2
+    of the License.
+
+    Grafx2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Grafx2; if not, see <http://www.gnu.org/licenses/> or
+    write to the Free Software Foundation, Inc.,
+    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
 #include <string.h>
 #include "vesalfb.h"
 
--- trunk/erreurs.h
+++ trunk/erreurs.h
@@ -1,4 +1,23 @@
-// Affcihenom fichier, n° ligne, nom fonction, une chaine et un entier.
+/*  Grafx2 - The Ultimate 256-color bitmap paint program
+
+    Copyright 2008 Adrien Destugues
+
+    Grafx2 is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; version 2
+    of the License.
+
+    Grafx2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Grafx2; if not, see <http://www.gnu.org/licenses/> or
+    write to the Free Software Foundation, Inc.,
+    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+// Affiche nom fichier, n° ligne, nom fonction, une chaine et un entier.
 #define DEBUG(y,z) printf("%s %d %s | %s : %d###\n",__FILE__,__LINE__,__func__,y,(unsigned int)z)
 
 // DEBUG en hexadécimal
