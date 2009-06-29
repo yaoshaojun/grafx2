b37e5cc3b3d954dcec60fe152aab55d70151c1e5
yrizoud <yrizoud@6a45ab37-182e-0410-b566-3f6d2655a21a>
2009-06-29 00:28:58 +0000

    Skin graphics and font are now recorded & reloaded.

--- readini.c
+++ readini.c
@@ -816,10 +816,15 @@ int Load_INI(T_Config * conf)
 
   // Optional, name of skin file. (>2.0)
   if(!Load_INI_get_string(file,buffer,"Skin_file",value_label,1))
-	  strcpy(conf->SkinFile,value_label);
+	  conf->Skin_file = strdup(value_label);
   else
-    strcpy(conf->SkinFile,"skin_modern.png");
+    conf->Skin_file = strdup("skin_modern.png");
 
+  // Optional, name of font file. (>2.0)
+  if(!Load_INI_get_string(file,buffer,"Font_file",value_label,1))
+	  conf->Font_file = strdup(value_label);
+  else
+    conf->Font_file = strdup("font_Classic.png");
 
   fclose(file);
 
