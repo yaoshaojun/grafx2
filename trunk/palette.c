a300ef500f74896a189208b3e023c30fe93ba8fb
yrizoud <yrizoud@6a45ab37-182e-0410-b566-3f6d2655a21a>
2010-01-25 19:15:25 +0000

    Palette operations that displace colors (sort, x-swap, x-inv, maybe others) now move around the transparent color index accordingly. The idea is that these changes don't modify 'What you see' of the image, and if the transp color was unchanged, it would be a huge visual change.

--- trunk/palette.c
+++ trunk/palette.c
@@ -234,6 +234,10 @@ void Remap_image_highlevel(byte * conversion_table)
   for (layer=0; layer<Main_backups->Pages->Nb_layers; layer++)
     Remap_general_lowlevel(conversion_table,Main_backups->Pages->Image[layer],Main_image_width,Main_image_height,Main_image_width);
 
+  // Remap transparent color
+  Main_backups->Pages->Transparent_color = 
+    conversion_table[Main_backups->Pages->Transparent_color];
+
   // On calcule les limites à l'écran de l'image
   if (Main_image_height>=Menu_Y_before_window)
     end_y=Menu_Y_before_window;
