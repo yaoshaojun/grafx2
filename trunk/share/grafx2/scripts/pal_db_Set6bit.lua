1d7cca85870d54103b3f156ab914c983dc76813e
yrizoud <yrizoud@6a45ab37-182e-0410-b566-3f6d2655a21a>
2010-03-21 15:43:11 +0000

    Lua scripts: Added the 'table' library. Added GPL headers to DawnBringer's scripts (thanks!). 2 cosmetic fixes in scripts.

--- trunk/share/grafx2/scripts/pal_db_Set6bit.lua
+++ trunk/share/grafx2/scripts/pal_db_Set6bit.lua
@@ -2,6 +2,13 @@
 --by Richard Fhager 
 --http://hem.fyristorg.com/dawnbringer/
 
+-- Copyright 2010 Richard Fhager
+--
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; version 2
+-- of the License. See <http://www.gnu.org/licenses/>
+
 
 -- Generate palette of all colors possible with a given number of shades for each channel
 -- 2 shades = 1 bit / channel = 3 bit palette = 2^3 colors =  8 colors
