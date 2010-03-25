5fe2c286c473c81044f700a9735d284ce9a257b0
yrizoud <yrizoud@6a45ab37-182e-0410-b566-3f6d2655a21a>
2010-03-25 20:12:17 +0000

    Merged changes from trunk r1374-r1375 to release

---
+++ branches/release/share/grafx2/scripts/pic_ni_GridIso.lua
@@ -0,0 +1,10 @@
+-- Draw isometric grid
+w,h=getpicturesize();
+for y=0,h-1,8 do
+  for x=0,w-1,1 do
+    putpicturepixel(x,y+(x/2)%8,1);
+    end;end
+for y=0,h-1,8 do
+  for x=0,w-1,1 do
+    putpicturepixel(x+3,y+7-((x/2)%8),1);
+    end;end
