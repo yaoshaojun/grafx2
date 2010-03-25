6777436863c3c5ef10967ae70b4c3d018130ea22
yrizoud <yrizoud@6a45ab37-182e-0410-b566-3f6d2655a21a>
2010-03-25 23:03:24 +0000

    [release] remove other files that were duplicated during merge

--- branches/release/scripts/pic_ni_PaletteX1.lua
+++
@@ -1,6 +0,0 @@
--- x1 palette to picture
-w,h=getpicturesize();
-for y1=0,7,1 do
-  for x1=0,31,1 do
-    putpicturepixel(x1,y1,y1+x1*8)
-    end;end
--- branches/release/scripts/pic_ni_PaletteX8.lua
+++
@@ -1,8 +0,0 @@
--- x8 palette to picture
-setpicturesize(256,64);
-for y1=0,7,1 do
-  for x1=0,31,1 do
-    for y2=0,7,1 do
-      for x2=0,7,1 do
-        putpicturepixel(x1*8+x2,y1*8+y2,y1+x1*8)
-        end;end;end;end
