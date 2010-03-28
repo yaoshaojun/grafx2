fa7c0c044239f2e8a3766c0a92f105b0ea09289c
yrizoud <yrizoud@6a45ab37-182e-0410-b566-3f6d2655a21a>
2010-03-28 00:31:57 +0000

    r1421 (Fix radio buttons) reported on release branch

--- branches/release/share/grafx2/scripts/_tst_Settings.lua
+++ branches/release/share/grafx2/scripts/_tst_Settings.lua
@@ -13,13 +13,13 @@ messagebox(
 
 
 ok, w, h = inputbox("Modify brush",
+  "RGB",    1, 0, 1,  -1,
+  "HSV",    0, 0, 1,  -1,
+  "HSL",    0, 0, 1,  -1,
   "Width",  w, -900.0,900.0, 3,
   "Height", h, -900.0,900.0, 4,
   "X Flip", 0, 0, 1,  0,
   "Y Flip", 0, 0, 1,  0,
-  "RGB",    1, 0, 1,  -1,
-  "HSV",    0, 0, 1,  -1,
-  "HSL",    0, 0, 1,  -1,
   "Degrees",1, 0, 1,  -2,
   "Radians",0, 0, 1,  -2  
 );
