93a504b6e547cc26c583a1be5eaf2527d50b0159
yrizoud <yrizoud@6a45ab37-182e-0410-b566-3f6d2655a21a>
2010-04-02 15:24:33 +0000

    Fix crash in inputbox when called with wrong arguments (negative precision for number input). Tweaked colors of icons

--- src/factory.c
+++ src/factory.c
@@ -577,6 +577,9 @@ int L_InputBox(lua_State* L)
     LUA_ARG_NUMBER(setting*args_per_setting+6, "inputbox", decimal_places[setting], -15.0, 15.0);
     if (decimal_places[setting]>15)
         decimal_places[setting]=15;
+    if (min_value[setting]!=0 || min_value[setting]!=1)
+      if (decimal_places[setting]<0)
+        decimal_places[setting]=0;
     // Keep current value in range
     if (decimal_places[setting]>=0)
       current_value[setting] = Fround(current_value[setting], decimal_places[setting]);
