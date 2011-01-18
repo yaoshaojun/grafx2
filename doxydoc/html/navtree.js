ec495df3a92f10d40b8fef216f1e2af44bc3c056
yrizoud <yrizoud@6a45ab37-182e-0410-b566-3f6d2655a21a>
2011-01-18 18:58:33 +0000

    Updated Doxygen documentation. Without the diagrams, because Graphviz crashes hard on Windows.

---
+++ doxydoc/html/navtree.js
@@ -0,0 +1,413 @@
+var NAVTREE =
+[
+  [ "GrafX2", "index.html", [
+    [ "Data Structures", "annotated.html", [
+      [ "mount_entry", "structmount__entry.html", null ],
+      [ "SFont_Font", "structSFont__Font.html", null ],
+      [ "T_Bitmap", "structT__Bitmap.html", null ],
+      [ "T_BMP_Header", "structT__BMP__Header.html", null ],
+      [ "T_Brush_template", "structT__Brush__template.html", null ],
+      [ "T_CEL_Header1", "structT__CEL__Header1.html", null ],
+      [ "T_CEL_Header2", "structT__CEL__Header2.html", null ],
+      [ "T_Cluster", "structT__Cluster.html", null ],
+      [ "T_Cluster_set", "structT__Cluster__set.html", null ],
+      [ "T_Color_cycle", "structT__Color__cycle.html", null ],
+      [ "T_Components", "structT__Components.html", null ],
+      [ "T_Config", "structT__Config.html", null ],
+      [ "T_Config_chunk", "structT__Config__chunk.html", null ],
+      [ "T_Config_header", "structT__Config__header.html", null ],
+      [ "T_Config_shortcut_info", "structT__Config__shortcut__info.html", null ],
+      [ "T_Config_video_mode", "structT__Config__video__mode.html", null ],
+      [ "T_Conversion_table", "structT__Conversion__table.html", null ],
+      [ "T_Dropdown_button", "structT__Dropdown__button.html", null ],
+      [ "T_Dropdown_choice", "structT__Dropdown__choice.html", null ],
+      [ "T_Fileselector", "structT__Fileselector.html", null ],
+      [ "T_Fileselector_item", "structT__Fileselector__item.html", null ],
+      [ "T_Font", "structT__Font.html", null ],
+      [ "T_Format", "structT__Format.html", null ],
+      [ "T_GIF_GCE", "structT__GIF__GCE.html", null ],
+      [ "T_GIF_IDB", "structT__GIF__IDB.html", null ],
+      [ "T_GIF_LSDB", "structT__GIF__LSDB.html", null ],
+      [ "T_Gradient", "structT__Gradient.html", null ],
+      [ "T_Gradient_array", "structT__Gradient__array.html", null ],
+      [ "T_Gradient_range", "structT__Gradient__range.html", null ],
+      [ "T_Gradient_set", "structT__Gradient__set.html", null ],
+      [ "T_Gui_skin", "structT__Gui__skin.html", null ],
+      [ "T_Help_section", "structT__Help__section.html", null ],
+      [ "T_Help_table", "structT__Help__table.html", null ],
+      [ "T_IMG_Header", "structT__IMG__Header.html", null ],
+      [ "T_IO_Context", "structT__IO__Context.html", null ],
+      [ "T_KCF_Header", "structT__KCF__Header.html", null ],
+      [ "T_Key_config", "structT__Key__config.html", null ],
+      [ "T_LBM_Header", "structT__LBM__Header.html", null ],
+      [ "T_List_button", "structT__List__button.html", null ],
+      [ "T_List_of_pages", "structT__List__of__pages.html", null ],
+      [ "T_Lookup", "structT__Lookup.html", null ],
+      [ "T_Menu_Bar", "structT__Menu__Bar.html", null ],
+      [ "T_Normal_button", "structT__Normal__button.html", null ],
+      [ "T_Occurrence_table", "structT__Occurrence__table.html", null ],
+      [ "T_Page", "structT__Page.html", null ],
+      [ "T_Paintbrush", "structT__Paintbrush.html", null ],
+      [ "T_Palette_button", "structT__Palette__button.html", null ],
+      [ "T_PCX_Header", "structT__PCX__Header.html", null ],
+      [ "T_PKM_Header", "structT__PKM__Header.html", null ],
+      [ "T_Polygon_edge", "structT__Polygon__edge.html", null ],
+      [ "T_Scroller_button", "structT__Scroller__button.html", null ],
+      [ "T_SCx_Header", "structT__SCx__Header.html", null ],
+      [ "T_Setting", "structT__Setting.html", null ],
+      [ "T_Shade", "structT__Shade.html", null ],
+      [ "T_Special_button", "structT__Special__button.html", null ],
+      [ "T_String_list", "structT__String__list.html", null ],
+      [ "T_Video_mode", "structT__Video__mode.html", null ],
+      [ "T_Window", "structT__Window.html", null ]
+    ] ],
+    [ "Data Structure Index", "classes.html", null ],
+    [ "Data Fields", "functions.html", null ],
+    [ "File List", "files.html", [
+      [ "brush.c", "brush_8c.html", null ],
+      [ "brush.h", "brush_8h.html", null ],
+      [ "brush_ops.c", "brush__ops_8c.html", null ],
+      [ "buttons.c", "buttons_8c.html", null ],
+      [ "buttons.h", "buttons_8h.html", null ],
+      [ "buttons_effects.c", "buttons__effects_8c.html", null ],
+      [ "const.h", "const_8h.html", null ],
+      [ "engine.c", "engine_8c.html", null ],
+      [ "engine.h", "engine_8h.html", null ],
+      [ "errors.h", "errors_8h.html", null ],
+      [ "factory.c", "factory_8c.html", null ],
+      [ "factory.h", "factory_8h.html", null ],
+      [ "fileformats.c", "fileformats_8c.html", null ],
+      [ "filesel.c", "filesel_8c.html", null ],
+      [ "filesel.h", "filesel_8h.html", null ],
+      [ "global.h", "global_8h.html", null ],
+      [ "graph.c", "graph_8c.html", null ],
+      [ "graph.h", "graph_8h.html", null ],
+      [ "haiku.cpp", "haiku_8cpp.html", null ],
+      [ "haiku.h", "haiku_8h.html", null ],
+      [ "help.c", "help_8c.html", null ],
+      [ "help.h", "help_8h.html", null ],
+      [ "helpfile.h", "helpfile_8h.html", null ],
+      [ "hotkeys.c", "hotkeys_8c.html", null ],
+      [ "hotkeys.h", "hotkeys_8h.html", null ],
+      [ "init.c", "init_8c.html", null ],
+      [ "init.h", "init_8h.html", null ],
+      [ "input.c", "input_8c.html", null ],
+      [ "input.h", "input_8h.html", null ],
+      [ "io.c", "io_8c.html", null ],
+      [ "io.h", "io_8h.html", null ],
+      [ "keyboard.c", "keyboard_8c.html", null ],
+      [ "keyboard.h", "keyboard_8h.html", null ],
+      [ "layers.c", "layers_8c.html", null ],
+      [ "layers.h", "layers_8h.html", null ],
+      [ "libraw2crtc.c", "libraw2crtc_8c.html", null ],
+      [ "libraw2crtc.h", "libraw2crtc_8h.html", null ],
+      [ "loadsave.c", "loadsave_8c.html", null ],
+      [ "loadsave.h", "loadsave_8h.html", null ],
+      [ "main.c", "main_8c.html", null ],
+      [ "misc.c", "misc_8c.html", null ],
+      [ "misc.h", "misc_8h.html", null ],
+      [ "miscfileformats.c", "miscfileformats_8c.html", null ],
+      [ "mountlist.c", "mountlist_8c.html", null ],
+      [ "mountlist.h", "mountlist_8h.html", null ],
+      [ "op_c.c", "op__c_8c.html", null ],
+      [ "op_c.h", "op__c_8h.html", null ],
+      [ "operatio.c", "operatio_8c.html", null ],
+      [ "operatio.h", "operatio_8h.html", null ],
+      [ "pages.c", "pages_8c.html", null ],
+      [ "pages.h", "pages_8h.html", null ],
+      [ "palette.c", "palette_8c.html", null ],
+      [ "palette.h", "palette_8h.html", null ],
+      [ "pversion.c", "pversion_8c.html", null ],
+      [ "pxdouble.c", "pxdouble_8c.html", null ],
+      [ "pxdouble.h", "pxdouble_8h.html", null ],
+      [ "pxquad.c", "pxquad_8c.html", null ],
+      [ "pxquad.h", "pxquad_8h.html", null ],
+      [ "pxsimple.c", "pxsimple_8c.html", null ],
+      [ "pxsimple.h", "pxsimple_8h.html", null ],
+      [ "pxtall.c", "pxtall_8c.html", null ],
+      [ "pxtall.h", "pxtall_8h.html", null ],
+      [ "pxtall2.c", "pxtall2_8c.html", null ],
+      [ "pxtall2.h", "pxtall2_8h.html", null ],
+      [ "pxtriple.c", "pxtriple_8c.html", null ],
+      [ "pxtriple.h", "pxtriple_8h.html", null ],
+      [ "pxwide.c", "pxwide_8c.html", null ],
+      [ "pxwide.h", "pxwide_8h.html", null ],
+      [ "pxwide2.c", "pxwide2_8c.html", null ],
+      [ "pxwide2.h", "pxwide2_8h.html", null ],
+      [ "readini.c", "readini_8c.html", null ],
+      [ "readini.h", "readini_8h.html", null ],
+      [ "readline.c", "readline_8c.html", null ],
+      [ "readline.h", "readline_8h.html", null ],
+      [ "realpath.c", "realpath_8c.html", null ],
+      [ "realpath.h", "realpath_8h.html", null ],
+      [ "saveini.c", "saveini_8c.html", null ],
+      [ "saveini.h", "saveini_8h.html", null ],
+      [ "sdlscreen.c", "sdlscreen_8c.html", null ],
+      [ "sdlscreen.h", "sdlscreen_8h.html", null ],
+      [ "setup.c", "setup_8c.html", null ],
+      [ "setup.h", "setup_8h.html", null ],
+      [ "SFont.c", "SFont_8c.html", null ],
+      [ "SFont.h", "SFont_8h.html", null ],
+      [ "shade.c", "shade_8c.html", null ],
+      [ "shade.h", "shade_8h.html", null ],
+      [ "special.c", "special_8c.html", null ],
+      [ "special.h", "special_8h.html", null ],
+      [ "struct.h", "struct_8h.html", null ],
+      [ "text.c", "text_8c.html", null ],
+      [ "text.h", "text_8h.html", null ],
+      [ "tiles.c", "tiles_8c.html", null ],
+      [ "transform.c", "transform_8c.html", null ],
+      [ "transform.h", "transform_8h.html", null ],
+      [ "version.c", "version_8c.html", null ],
+      [ "windows.c", "windows_8c.html", null ],
+      [ "windows.h", "windows_8h.html", null ]
+    ] ],
+    [ "Globals", "globals.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
