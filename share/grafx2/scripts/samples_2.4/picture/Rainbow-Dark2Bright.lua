a376a82d9ca60eb1a68440bf7a686f95034e03d4
yrizoud <yrizoud@6a45ab37-182e-0410-b566-3f6d2655a21a>
2012-09-28 00:04:55 +0000

    Upgrate Lua sample scripts to DawnBringer's 1.3 toolbox

--- share/grafx2/scripts/samples_2.4/picture/Rainbow-Dark2Bright.lua
+++ share/grafx2/scripts/samples_2.4/picture/Rainbow-Dark2Bright.lua
@@ -1,47 +1,14 @@
---PICTURE: Rainbow - Dark to Bright
+--PICTURE: Rainbow - Dark to Bright v1.1
 --by Richard Fhager 
 --http://hem.fyristorg.com/dawnbringer/
 -- Email: dawnbringer@hem.utfors.se
 -- MSN:   annassar@hotmail.com
 --
--- Copyright 2010 Richard Fhager
---
--- This program is free software; you can redistribute it and/or
--- modify it under the terms of the GNU General Public License
--- as published by the Free Software Foundation; version 2
--- of the License. See <http://www.gnu.org/licenses/>
 
--- This script was adopted from Evalion, a Javascript codecrafting/imageprocessing project
--- http://goto.glocalnet.net/richard_fhager/evalion/evalion.html 
---
 
---
-function shiftHUE(r,g,b,deg) -- V1.3 R.Fhager 2007, adopted from Evalion
- local c,h,mi,mx,d,s,p,i,f,q,t
- c = {g,b,r}
- mi = math.min(r,g,b)
- mx = math.max(r,g,b); v = mx;
- d = mx - mi;
- s = 0; if mx ~= 0 then s = d/mx; end
- p = 1; if g ~= mx then p = 2; if b ~= mx then p = 0; end; end
- 
- if s~=0 then
-  h=(deg/60+(6+p*2+(c[1+p]-c[1+(p+1)%3])/d))%6;
-  i=math.floor(h);
-  f=h-i;
-  p=v*(1-s);
-  q=v*(1-s*f);
-  t=v*(1-s*(1-f));
-  c={v,q,p,p,t,v}
-  r = c[1+i]
-  g = c[1+(i+4)%6]
-  b = c[1+(i+2)%6]
- end
-
- return r,g,b
-end
---
-
+--dofile("dawnbringer_lib.lua") 
+run("../libs/dawnbringer_lib.lua")
+--> db.shiftHUE(r,g,b, deg)
 
 w, h = getpicturesize()
 
@@ -56,13 +23,14 @@ for y = 0, h - 1, 1 do
    g = (oy-0.5)*512 * oy
    b = (oy-0.5)*512 * oy
 
-   r, g, b = shiftHUE(r,g,b,ox * 360); 
+   r, g, b = db.shiftHUE(r,g,b,ox * 360); 
  
-   c = matchcolor(math.max(0,math.min(255,r)),math.max(0,math.min(255,g)),math.max(0,math.min(255,b)))
+   c = matchcolor(r,g,b)
  
    putpicturepixel(x, y, c);
 
   end
+  updatescreen(); if (waitbreak(0)==1) then return; end
 end
 
 
