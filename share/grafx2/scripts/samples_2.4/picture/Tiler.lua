2b0db6bfd57d45795945deebbaf85d8745323770
pulkomandy <pulkomandy@6a45ab37-182e-0410-b566-3f6d2655a21a>
2013-03-03 16:18:14 +0000

    Add some useful scripts :  - ThomsonConstraints is yet another constraint checker  - FontConvert extracts chars from a font as binary data

--- share/grafx2/scripts/samples_2.4/picture/Tiler.lua
+++ share/grafx2/scripts/samples_2.4/picture/Tiler.lua
@@ -2,7 +2,7 @@
 --Extract unique tiles from the spare page
 --to the main one. Main page is erased.
 --
--- Copyright 2011 Adrien Destugues <pulkomandy@pulkomandy.ath.cx>
+-- Copyright 2011 Adrien Destugues <pulkomandy@pulkomandy.tk>
 --
 -- This program is free software; you can redistribute it and/or
 -- modify it under the terms of the GNU General Public License
